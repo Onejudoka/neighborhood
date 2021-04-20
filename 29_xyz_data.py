f997410cff5a1bf5f2fbc13b1f7e3aba58b08b8e
Onejudoka <75645699+Onejudoka@users.noreply.github.com>
2021-04-20 10:19:28 -0700

    Add files via upload

---
+++ 29_xyz_data.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import sys
+import cv2
+import depthai as dai
+import numpy as np
+import time
+
+
+......................
+labelMap = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
+            "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
